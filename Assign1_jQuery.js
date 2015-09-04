8537bb3bc000cfddd00ef0584666dbe3011e2961
Ariana Loza <akloza@ucdavis.edu>
2015-09-03 17:24:14 -0700

    added all files and pics to repo

--- Assign1_jQuery.js
+++ Assign1_jQuery.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 
-	$(".btn0").click(function(){
-		$("#p1").addClass("statement");
-	});
+	
 	$
 	$(".btn1").click(function(){
 		$("li").show();
@@ -15,12 +13,35 @@ $(document).ready(function(){
 	$("#p3").before("<b>How to Style:</b>");
 	$("h3").text("Mix and Match!");
 	$("#naturalHues").fadeOut(2000);
+	$("#mixAndMatch").fadeIn("slow");
+	$('.toggle_shoe1').click(function(){
+	$("#shoe1").toggle(2000);
 
-	$(".btn3").click(function(){
-		if($('option').val()==="1"){
+
+	});
+	$('.slideDown_shoe2').click(function(){
+		$("#shoe2").slideDown();
+	});
+	
+
+	/*$(".btn3").click(function(){
+		switch (option (). getVal()){
+			case ($('option').val()==="1"){
 			$('#color').append("yellow");
 		}
-		
+			case ($('option').val()==="2"){
+				$('#color').append("beige");
+			}
+			case ($('option').val()==="3"){
+				$('#color').append("blush");
+			}
+			case($('option').val()==="4"){
+				$('color').append("white");
+			}
+			case($('option').val()==="5"){
+				$('color').append("black")
+			}
+			default: $('color').append("choose an age")
 		});
-		
+		});*/
 });
